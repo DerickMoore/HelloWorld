ff1054f6feaa83b1de775fa77e05e38bc869feee
DerickMoore <32001270+DerickMoore@users.noreply.github.com>
2023-07-14 22:54:36 +0100

    Change after install GitHub client
    Add launch.json to project

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,11 @@
 def dm_hello(s):
     print(s)
-    
+
+
 dm_hello("Hello, world again!")
 
 print("Second print")
 print("Change made locally under source control")
 print("Second change made through Visual Studio Code")
 print("Third change made through Visual Studio Code")
+print("Fourth change made through VS COde")
