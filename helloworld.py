e0d136a9396a57f8073c795c4aafa3bce4420ead
DerickMoore <32001270+DerickMoore@users.noreply.github.com>
2026-02-22 16:18:57 +0000

    Update helloworld.py
    Clean up prints

--- helloworld.py
+++ helloworld.py
@@ -2,10 +2,5 @@ def dm_hello(s):
     print(s)
 
 
-dm_hello("Hello, world again!")
+dm_hello("Hello, world !")
 
-print("Second print")
-print("Change made locally under source control")
-print("Second change made through Visual Studio Code")
-print("Third change made through Visual Studio Code")
-print("Fourth change made through VS COde")
