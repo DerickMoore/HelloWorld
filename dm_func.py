da3d7d1330d37810025bc1386690276ec5160a1b
DerickMoore <32001270+DerickMoore@users.noreply.github.com>
2026-03-09 19:31:12 +0000

    Move prime function

--- dm_func.py
+++ dm_func.py
@@ -8,3 +8,25 @@ def dm_hello(s):
         s: String to print
     """
     print(s)
+
+def is_prime(n):
+    """Check if a number is prime.
+
+    Args:
+        n: Integer to check
+
+    Returns:
+        True if n is prime, False otherwise
+    """
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
